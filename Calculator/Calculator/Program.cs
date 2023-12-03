f44d0f61134804f0456e28203d0fa97cdb4c4080
gazzz07 <124935058+gazzz07@users.noreply.github.com>
2023-12-03 00:02:54 +0000

    adding count of operations and list

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,106 +1,82 @@
-﻿class Calculator
+﻿using CalculatorLibrary;
+namespace CalculatorProgram
 {
-    public static double DoOperation(double num1, double num2, string op)
+    class Program
     {
-        double result = double.NaN;
-
-
-        switch (op)
+        static void Main(string[] args)
         {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num2 - num1;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
+            int timesUsed = 0;
+            bool endApp = false;
 
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("--------------------------");
 
-            default:
-                break;
-        }
-        return result;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        bool endApp = false;
-
-        Console.WriteLine("Console Calculator in C#\r");
-        Console.WriteLine("--------------------------");
-
-        while (!endApp)
-        {
-
-            string numInput1 = "";
-            string numInput2 = "";
-            double result = 0;
-
-
-            Console.WriteLine("Type a number, and then press Enter: ");
-            numInput1 = Console.ReadLine();
-
-            double cleanNum1 = 0;
-            while (!double.TryParse(numInput1, out cleanNum1))
+            Calculator calculator = new Calculator();
+            while (!endApp)
             {
-                Console.WriteLine("This is not a valid input. Please enter an integer: ");
+                
+
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+
+                Console.WriteLine("Type a number, and then press Enter: ");
                 numInput1 = Console.ReadLine();
-            }
 
-
-            Console.WriteLine("Type another number, and then press Enter: ");
-            numInput2 = Console.ReadLine();
-
-            double cleanNum2 = 0;
-            while (!double.TryParse(numInput2, out cleanNum2))
-            {
-                Console.WriteLine("This is not a valid input. Please enter an integer: ");
-                numInput2 = Console.ReadLine();
-            }
-
-
-            Console.WriteLine("Choose an operator from the following list: ");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.WriteLine("Your option?: ");
-
-            string op = Console.ReadLine();
-
-            try
-            {
-                result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
-                if (double.IsNaN(result))
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
                 {
-                    Console.WriteLine("This operation will result in a mathematical error. \n");
+                    Console.WriteLine("This is not a valid input. Please enter an integer: ");
+                    numInput1 = Console.ReadLine();
                 }
-                else Console.WriteLine("Your result: {0:0.##}\n", result);
+
+
+                Console.WriteLine("Type another number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.WriteLine("This is not a valid input. Please enter an integer: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+
+                Console.WriteLine("Choose an operator from the following list: ");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("Your option?: ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error. \n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the maths.\n - Details: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------");
+
+                timesUsed++;
+                Console.WriteLine($"Calculator has been used {timesUsed} times in this session.");
+                Console.WriteLine("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Oh no! An exception occurred trying to do the maths.\n");
-            }
-
-            Console.WriteLine("------------------------");
-
-
-            Console.WriteLine("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-            if (Console.ReadLine() == "n") endApp = true;
-
-            Console.WriteLine("\n");
+            calculator.Finish();
+            return;
         }
-        return;
     }
 }
--- Calculator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87b555058f1fb60e3dc6ecbf0b6da3cbf8351c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6756c2c6d239e9bf1c91b8fba466d0421c5850")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
